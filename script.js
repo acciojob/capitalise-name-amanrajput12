f5ddfa837918810af58be70204b5d39cb00bed2b
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-01-30 12:19:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const input = document.querySelector('#fname');
+
+input.addEventListener("focusout",(e)=>{
+    // alert(e.target.value.toUpperCase());
+	let data = e.target.value.toUpperCase();
+			input.value=data
+})
